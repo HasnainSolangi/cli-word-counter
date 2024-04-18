40e3166e59cf24f7acd5f725007ebe723396f181
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-04-19 01:59:11 +0500

    Add files via upload

---
+++ wc.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+// This line is called a Shebang, it tells the OS to run it with node.js
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Welcome message for the user
+console.log(chalk.green("Welcome! Let's count the words in your sentence."));
+const answers :{Sentence:string} = await inquirer.prompt
+
+(      
+    [   
+        {
+            name:"Sentence",
+            type:"input",
+            message:"Enter your Sentence to count the words",
+
+        }
+    ]
+);
+
+
+const word=answers.Sentence.trim().split(" ")
+console.log(word);
+
+console.log(`Your Sentence word count is ${word.length}`);
+console.log(chalk.yellow("Thank you for using our word counter. See you soon!"));
